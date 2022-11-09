fc4c37cc18fb409f6e64b809afb2b2af3811c806
Iago Yanni <99916462+iagoyanni015@users.noreply.github.com>
2022-11-08 22:54:29 -0300

    complete challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,25 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        const userAdmin = this.usersRepository.findById(user_id);
+
+        if (!userAdmin) {
+            throw new Error("User not Found");
+        }
+
+        if (!userAdmin.admin) {
+            throw new Error("User is not a Admin!");
+        }
+
+        return this.usersRepository.list();
+    }
 }
 
 export { ListAllUsersUseCase };
